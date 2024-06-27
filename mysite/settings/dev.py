b6d361bf708a90a82d090696b7a0ccaf1ef28ab0
wanjiru-njuguna <missnjuguna19@gmail.com>
2024-06-27 19:45:49 +0300

    portfolio webbsite using wagtail

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ml4fwrlx^@4nvpn(i790)q1ci**^o+x676@fqbt6k3usop4)h!"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ portfolio/blocks.py
@@ -0,0 +1,29 @@
+from base.blocks import BaseStreamBlock
+from wagtail.blocks import (
+    CharBlock,
+    ListBlock,
+    PageChooserBlock,
+    RichTextBlock,
+    StructBlock,
+)
+from wagtail.images.blocks import ImageChooserBlock
+
+class CardBlock(StructBlock):
+    heading = CharBlock()
+    text = RichTextBlock(features=["bold", "italic", "link"])
+    image = ImageChooserBlock(required=False)
+
+    class Meta:
+        icon = "form"
+        template = "portfolio/blocks/card_block.html"
+class FeaturedPostsBlock(StructBlock):
+    heading = CharBlock()
+    text = RichTextBlock(features=["bold", "italic", "link"], required=False)
+    posts = ListBlock(PageChooserBlock(page_type="blog.BlogPage"))
+
+    class Meta:
+        icon = "folder-open-inverse"
+        template = "portfolio/blocks/featured_posts_block.html"
+class PortfolioStreamBlock(BaseStreamBlock):
+    card = CardBlock(group="Sections")
+    featured_posts = FeaturedPostsBlock(group="Sections")
